8c838ccfd1028003b67e32a6518fe85cfd590115
ryan <123456>
2023-07-26 21:14:42 +0800

    第二课

--- js/index.js
+++ js/index.js
@@ -1,79 +1,74 @@
 /**
- * 1.注册谷歌账号，搜藏书签：https://www.w3school.com.cn/
- * https://cn.vuejs.org/
- * 2.JS数据类型
- * 3.JS基础语法
- * 4.JS操作html dom
- * 5.JS console 输出方便调试
+ * 第二课 数组、对象、函数（方法）
  */
 
-// 数据类型字符串
-var name1='bruce';
-// 数据类型number
-let num = 12;
-// 数据类型boolean
-// const 表示常量，不可改变数据
-const isDebug = false;
-// 数据类型undefined
-let v1;
-// 数据类型object
-let v2=null;
-// 数据类型object
-let obj={};
-// 数据类型function
-let fn=function(){
-    console.log('this is a function');
+// 复习
+// 获取元素(也叫标签或者节点)
+document.querySelector('.right') 
+document.querySelector('#id') 
+document.getElementsByTagName('body')
+
+let arr1=[1,2,3,4];
+let arr2 = ['bruce', 'mongo']
+let arr3 = ['bruce', 'mongo', 323,555]
+
+console.log('数组', arr1,arr2,arr3)
+
+let nam = document.querySelector('.right') 
+// 获取输入框的值，是先获取元素，然后获取value
+console.log(nam.value)
+
+
+// 对象
+let obj = {
+    name:'bruce',
+    sex:'男',
+    weight:50,
+}
+console.log(obj, '对象-姓名', obj.name)
+// 对象赋值
+obj.name = 'mongo'
+obj['sex'] = 46;
+console.log('改变后姓名', obj.name)
+
+for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+        console.log(obj[key]);        
+    }
 }
 
-console.log('hello word')
-console.log('name1', name1)
-console.log('num', num)
-console.log('isDebug',isDebug)
+console.log('obj索引取值', obj['sex'], obj.name)
 
-console.log('name1:%o, num:%o, v1:%o,v2:%o,', name1, num, isDebug,v1,v2,fn)
-console.log('fn数据类型', typeof fn)
-// isDebug = true;
-// console.log('isDebug',isDebug)
+// function 函数/方法
 
-var x, y;	// 如何声明变量
-x = '7'; y = 8;	// 如何赋值
-zzzz = x + y;	// 如何计算值
-
-console.log('z', zzzz)
-console.log('"78"是否等于78', zzzz==78)
-console.log('0是否等于false', 0==false)
-console.log('0是否等于false', 0===false)
-
-console.log('0不等于false', 0!=false)
-console.log('0不等于false', 0!==false)
-
-
-/* 
-JS 语法
-JavaScript 对大小写敏感
-JavaScript 使用算数运算符（+ - * /）来计算值：
-*/
-
-if(x==y){
-    console.log('x==y')
-}else{
-    console.log('x!=y')
+// 无返回值
+function add(a,b,c){
+    // console.log('参数', a,b,c)
+    let sum = a+b+c;
+    console.log('结果', sum)       
 }
 
-switch (x) {
-    case '1':
-        console.log('进入1')
-        break;
-        case '2':
-        console.log('进入2')
-        break;
-    default:
-        console.log('进入默认')
-        break;
+// NaN 非数字 typeof NaN的结果为number
+add(1, 2);
+
+add(1, 2, 5);
+
+add(1, 2, '5');
+
+// 有返回值
+function add2(a, b){
+    let sum = a + b;
+    return sum      
 }
 
-for (let i = 0; i < x; i++) {
-    console.log('i', i)
-}
+let res = add2(4, 9)
 
-document.getElementById("app").innerHTML = name1
+console.log('add2', res)
+
+function show(){
+    let namea1 = document.querySelector('.right') .value
+    let namea2 = document.querySelector('.left') .value
+    console.log("namea1的值为%o,namea2的值为%o",namea1,namea2)
+    let res = `姓名：${namea1},性别：${namea2}`
+    alert(res)
+}
